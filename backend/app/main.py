5f559afd189a36f53cd212eed21c9fb50ce68c32
Carlos Gutiérrez Sánchez <104018258+KARINKILLER@users.noreply.github.com>
2025-03-10 20:14:37 +0100

    ahora se crea la conexión a la base de datos cada vez que se recarga la aplicación y primeros endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
+from dbHelper import *
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await init_db()
+        yield
+    finally:
+        await close_db()
+
+app = FastAPI(lifespan=lifespan)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,3 +23,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.post("/login")
+async def login(username: str = Body(...), password: str = Body(...)):
+    try:
+        login_valido = await validarLogin(username, password)
+        if login_valido:
+            #Crear el JWT
+            return {"message": "Inicio de sesión exitoso", "username": username}
+        else:
+            raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+    except Exception as e:
+        print(f"Error en la ruta /login: {e}")
+        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+
+
+@app.post("/register")
+async def register(email: str = Body(...), username: str = Body(...), password: str = Body(...)):
+    print("Hemos entrado en la función register!!")
+    try:
+        registrado = await registrarUsuario(email, username, password)
+        return {"message": "Usuario registrado correctamente", "username": username}
+    except Exception as e:
+        print(f"Error en la ruta /register: {e}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+    
