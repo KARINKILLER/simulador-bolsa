0f32539f2e31d917ab30a1f242d59bca49296d74
Carlos Gutiérrez Sánchez <104018258+KARINKILLER@users.noreply.github.com>
2025-03-19 12:05:32 +0100

    añadido el componente gráfica, para ver historial de precios y componente modal, para la compra de acciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './componentes/Login';
+import Register from './componentes/Register';
+import Market from './componentes/Market';
+import Perfil from './componentes/Perfil';
+import Transacciones from './componentes/Transacciones';
+import Help from './componentes/Help';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/market" element={<Market />} />
         <Route path="/perfil" element={<Perfil />} />
+        <Route path="/help" element={<Help />} />
         <Route path="/transacciones" element={<Transacciones />} />
       </Routes>
     </Router>
